78ae25e5bbe50b1887aa3d3116e8225743e068a0
Yuri Gorokhov <yurigorokhov@gmail.com>
2015-03-12 23:55:51 -0700

    Fixing tests

--- without_test.go
+++ without_test.go
@@ -3,22 +3,16 @@ package goplug_test
 import (
 	plug "github.com/yurigorokhov/goplug"
 	. "gopkg.in/check.v1"
-	"testing"
 )
 
 // Hook up gocheck into the "go test" runner.
-func Test(t *testing.T) { TestingT(t) }
-
-type GoPlugSuite struct{}
-
-var _ = Suite(&GoPlugSuite{})
-
-func (s *GoPlugSuite) Test_with_adds_parameters(c *C) {
+func (s *GoPlugSuite) Test_without_removes_parameters(c *C) {
 	p, err := plug.New("http://test.dev")
 	c.Assert(err, IsNil)
 	p.
 		With("param1", "value1").
-		With("param2", "value2")
+		With("param2", "value2").
+		Without("param2")
 	c.Assert(p.GetParam("param1"), Equals, "value1")
-	c.Assert(p.GetParam("param2"), Equals, "value2")
+	c.Assert(p.GetParam("param2"), Equals, "")
 }
